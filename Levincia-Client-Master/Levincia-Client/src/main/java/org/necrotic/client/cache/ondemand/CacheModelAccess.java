20371cec8a67ebbf2448c2f8646aabcbe4d46de2
Levinvia <128869979+hankrsps@users.noreply.github.com>
2026-08-17 20:34:53 -0400

    Remove unused cache model helper

--- Levincia-Client-Master/Levincia-Client/src/main/java/org/necrotic/client/cache/ondemand/CacheModelAccess.java
+++
@@ -1,9 +0,0 @@
-package org.necrotic.client.cache.ondemand;
-
-/**
- * Small bridge that lets the model loader read model bytes directly from the
- * packed cache before falling back to asynchronous on-demand requests.
- */
-public interface CacheModelAccess {
-    byte[] getModelData(int modelId);
-}
