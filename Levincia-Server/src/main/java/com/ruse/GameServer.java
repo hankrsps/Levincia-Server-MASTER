e094c8c6fa2c643e3ecfacc7488e19e402e61f57
Levinvia <128869979+hankrsps@users.noreply.github.com>
2026-08-23 20:35:33 -0400

    Make server startup independent of IntelliJ working directory

--- Levincia-Server/src/main/java/com/ruse/GameServer.java
+++ Levincia-Server/src/main/java/com/ruse/GameServer.java
@@ -2,6 +2,8 @@ package com.ruse;
 
 import com.ruse.util.ShutdownHook;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,12 +17,42 @@ import java.util.logging.Logger;
  */
 public class GameServer {
 
+    static {
+        normalizeWorkingDirectory();
+    }
+
     private static final GameLoader loader = new GameLoader(GameSettings.GAME_PORT);
     private static final Logger logger = Logger.getLogger("Levincia");
     private static boolean updating;
+
+    private static void normalizeWorkingDirectory() {
+        File cwd = new File(System.getProperty("user.dir"));
+        if (new File(cwd, "data").isDirectory()) {
+            return;
+        }
+
+        File nestedServer = new File(cwd, "Levincia-Server");
+        if (new File(nestedServer, "data").isDirectory()) {
+            System.setProperty("user.dir", nestedServer.getAbsolutePath());
+            return;
+        }
+
+        try {
+            File classes = new File(GameServer.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+            File target = classes.getParentFile();
+            File serverRoot = target != null ? target.getParentFile() : null;
+            if (serverRoot != null && new File(serverRoot, "data").isDirectory()) {
+                System.setProperty("user.dir", serverRoot.getAbsolutePath());
+            }
+        } catch (URISyntaxException | SecurityException ignored) {
+            // If auto-detection fails, startup will report the original missing-data error.
+        }
+    }
+
     public static void main(String[] params) {
         Runtime.getRuntime().addShutdownHook(new ShutdownHook());
         try {
+            logger.info("Working directory: " + System.getProperty("user.dir"));
             logger.info("Initializing the loader...");
             loader.init();
             loader.finish();
